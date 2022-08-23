cd6e4e18511b4b0dc5ded2bfad7dd099e2690dfc
Kukhianidze <KUKH004@dx1.admgt.net>
2022-08-23 15:15:03 +0400

    Creating a Basic Attribute Directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
-
-import { AppComponent } from './app.component';
-
+import { AppComponent } from "./app.component";
+import { BasicHighLightDirective } from "./basic-highlight/basic-highlight.directive";
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-  ],
+  declarations: [AppComponent, BasicHighLightDirective],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
